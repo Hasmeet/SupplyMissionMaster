ad3b3daceaae5b9284ae79bc94a23a8922327d73
Hasmeet <73887428+Hasmeet@users.noreply.github.com>
2020-12-08 19:38:24 +0530

    Add files via upload

---
+++ SupplyMission-master/sketch.js
@@ -0,0 +1,79 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+var r1,r2,r3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2;
+	
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	r1 = createSprite (width/2.35,620,10,100);
+	r1.shapeColor= "red";
+
+	r2 = createSprite (width/1.75,620,10,100);
+	r2.shapeColor= "red";
+
+	r3 = createSprite (width/2,660,120,10);
+	r3.shapeColor= "red";
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
+	World.add(world, packageBody,helicopterSprite);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  keyPressed();
+  r1.display();
+  r2.display();
+  r3.display();
+    packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+    Matter.Body.setStatic(packageBody,false);	
+  }
+}
+
+
+
